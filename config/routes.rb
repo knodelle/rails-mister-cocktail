0ea0ed63946140358d3d1560df24255a12c5e9b0
Marie Begue <marie.prandstetter@gmail.com>
2019-11-15 14:31:37 +0100

    controllers and view  added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'reviews/review_params'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'doses/doses_params'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/cocktail_params'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
